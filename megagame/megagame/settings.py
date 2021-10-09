fd48f643ab8fcf36aec85e6b487131f00c05337b
PearlFoe <itknml@gmail.com>
2021-10-09 12:22:32 +0300

    Перенес медиафайлы в отдельную папку к static файлам, добавил html и сss файлы
    Избавился от проблемы, которая не давала подгружать лого на всех
    страницах, кроме главной.
    Добавил html страницы для events.

--- megagame/megagame/settings.py
+++ megagame/megagame/settings.py
@@ -131,6 +131,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
