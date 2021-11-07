d3039e8d238e8d0f1688680407cafc2038a80e8b
PearlFoe <itknml@gmail.com>
2021-11-07 16:33:57 +0300

    Поправил формат вывода даты события, доработал стили катрочки события

--- megagame/megagame/settings.py
+++ megagame/megagame/settings.py
@@ -61,8 +61,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-LANGUAGE_CODE = 'ru-ru'
-
 ROOT_URLCONF = 'megagame.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -122,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
