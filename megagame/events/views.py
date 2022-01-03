74bfd1828c28825a434eab669cc1114652890dd9
Denis Voronkin <vd@x-lab.com.ru>
2022-01-03 15:15:52 +0300

    Доделал автообновление ленты нововстей

--- megagame/events/views.py
+++ megagame/events/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from django.core.exceptions import ObjectDoesNotExist
+from django.core import serializers
 
 from events.models import Event, Article
 from events.forms import ArticleForm
@@ -64,11 +65,10 @@ def get_new_articles(request, event_id: int, last_article_id: int=None) -> JsonR
 	try:
 		last_article = Article.objects.filter(id=last_article_id).get()
 	except Exception:
-		new_articles = Article.objects.filter(event=event).in_bulk()
+		new_articles = Article.objects.filter(event=event)
 	else:
-		new_articles = Article.objects.filter(event=event, creation_date__gt=last_article.creation_date).in_bulk()
-
-	return JsonResponse({'success': True, 'artiсles': new_articles}, status=201)
+		new_articles = Article.objects.filter(event=event, creation_date__gt=last_article.creation_date)
+	return JsonResponse({'success': True, 'articles': [article.as_dict() for article in new_articles]}, status=200)
 
 @login_required(login_url='login_page')
 def create_new_article(request, event_id: int) -> JsonResponse:
